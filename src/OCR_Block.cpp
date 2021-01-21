ce93374e529a5836f2628e2e8b3c189c51dd9340
Shiva Kumar H R <shivahr@gmail.com>
2021-01-21 17:50:22 +0530

    Separate wcharToUTF code to a separate function

--- src/OCR_Block.cpp
+++ src/OCR_Block.cpp
@@ -160,29 +160,36 @@ void OCR_Block::saveSegmentedLines(unsigned int blockNum) {
 	}
 }
 
-ofstream &operator<<(ofstream &out, vector<wchar_t> &unicodes) {
+unsigned int wcharToUTF8(wchar_t unicode, unsigned char* ch) {
 	union {
 		unsigned char unsignedCharValue[2];
 		unsigned short int unsignedIntValue;
 	} adapter;
-	unsigned char ch;
+	adapter.unsignedCharValue[0] = unicode % 0X100;
+	adapter.unsignedCharValue[1] = unicode / 0X100;
+	unsigned int length;
+	if (adapter.unsignedIntValue < 0x0080) {
+		ch[0] = adapter.unsignedCharValue[0];
+		length = 1;
+	} else if (adapter.unsignedIntValue < 0x0800) {
+		ch[0] = 0xc0 | ((adapter.unsignedIntValue) >> 6);
+		ch[1] = 0x80 | ((adapter.unsignedIntValue) & 0x3f);
+		length = 2;
+	} else {
+		ch[0] = 0xe0 | ((adapter.unsignedIntValue) >> 12);
+		ch[1] = 0x80 | (((adapter.unsignedIntValue) >> 6) & 0x3f);
+		ch[2] = 0x80 | ((adapter.unsignedIntValue) & 0x3f);
+		length = 3;
+	}
+	return length;
+}
+
+ofstream &operator<<(ofstream &out, vector<wchar_t> &unicodes) {
+	unsigned char ch[3];
 	for (unsigned int i = 0; i < unicodes.size(); i++) {
-		adapter.unsignedCharValue[0] = unicodes[i] % 0X100;
-		adapter.unsignedCharValue[1] = unicodes[i] / 0X100;
-		if (adapter.unsignedIntValue < 0x0080) {
-			out << adapter.unsignedCharValue[0];
-		} else if (adapter.unsignedIntValue < 0x0800) {
-			ch = 0xc0 | ((adapter.unsignedIntValue) >> 6);
-			out << ch;
-			ch = 0x80 | ((adapter.unsignedIntValue) & 0x3f);
-			out << ch;
-		} else {
-			ch = 0xe0 | ((adapter.unsignedIntValue) >> 12);
-			out << ch;
-			ch = 0x80 | (((adapter.unsignedIntValue) >> 6) & 0x3f);
-			out << ch;
-			ch = 0x80 | ((adapter.unsignedIntValue) & 0x3f);
-			out << ch;
+		unsigned int len = wcharToUTF8(unicodes[i], ch);
+		for (unsigned int j = 0; j < len; j++) {
+			out << ch[j];
 		}
 	}
 	return out;
