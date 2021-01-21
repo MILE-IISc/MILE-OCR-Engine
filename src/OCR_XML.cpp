eeb1daebd6360c0747a22e67acab4e0b68f3f322
Shiva Kumar H R <shivahr@gmail.com>
2021-01-21 19:23:02 +0530

    Save bounding box co-ordinates in XML

--- src/OCR_XML.cpp
+++ src/OCR_XML.cpp
@@ -15,25 +15,39 @@ void writeOcrOutputXML(OCR_Page &page, const char* outputXmlPath) {
 	doc->InsertEndChild(pageElement);
 
 	vector<OCR_Block> &blocks = page.blocks;
+	int lineNumber = 1;
 	for (unsigned int b = 0; b < blocks.size(); b++) {
 		XMLElement* blockElement = pageElement->InsertNewChildElement("block");
-		vector<OCR_Line> &lines = blocks[b].lines;
+		OCR_Block &block = blocks[b];
+		vector<OCR_Line> &lines = block.lines;
 		for (unsigned int l = 0; l < lines.size(); l++) {
 			XMLElement* lineElement = blockElement->InsertNewChildElement("line");
-			vector<OCR_Word> &words = lines[l].words;
+			OCR_Line &line = lines[l];
+			vector<OCR_Word> &words = line.words;
 			for (unsigned int w = 0; w < words.size(); w++) {
 				XMLElement* wordElement = lineElement->InsertNewChildElement("word");
-				vector<OCR_Akshara> &aksharas = words[w].aksharas;
+				OCR_Word &word = words[w];
+				vector<OCR_Akshara> &aksharas = word.aksharas;
 				vector<wchar_t> unicodes;
 				for (unsigned int a = 0; a < aksharas.size(); a++) {
 					copyVector(aksharas[a].unicodes, unicodes);
 				}
 				wordElement->SetAttribute("unicode", toString(unicodes).c_str());
 				wordElement->SetAttribute("WordNumber", w + 1);
+				wordElement->SetAttribute("rowStart", line.lineTop);
+				wordElement->SetAttribute("rowEnd", line.lineBottom);
+				wordElement->SetAttribute("colStart", word.xStart);
+				wordElement->SetAttribute("colEnd", word.xEnd);
 			}
-			lineElement->SetAttribute("LineNumber", l + 1);
+			lineElement->SetAttribute("LineNumber", lineNumber++);
+			lineElement->SetAttribute("rowStart", line.lineTop);
+			lineElement->SetAttribute("rowEnd", line.lineBottom);
 		}
 		blockElement->SetAttribute("BlockNumber", b + 1);
+		blockElement->SetAttribute("rowStart", block.boundingBox.y);
+		blockElement->SetAttribute("rowEnd", block.boundingBox.y + block.boundingBox.height - 1);
+		blockElement->SetAttribute("colStart", block.boundingBox.x);
+		blockElement->SetAttribute("colEnd", block.boundingBox.x + block.boundingBox.width - 1);
 	}
 
 	doc->SaveFile(outputXmlPath);
