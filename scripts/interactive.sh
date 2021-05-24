11dbee616c5182d2eb192a4b0787643b1ff4fb67
Anil Kumar K K <anilkumar911@gmail.com>
2021-05-24 12:43:49 +0530

    add --rm for docker run

--- scripts/interactive.sh
+++ scripts/interactive.sh
@@ -1,1 +1,1 @@
-docker run -it --name mile-ocr-engine-dev --net=host -v $PWD:/home/ocr mile-ocr-engine-dev bash
+docker run --rm -it --name mile-ocr-engine-dev --net=host -v $PWD:/home/ocr mile-ocr-engine-dev bash
