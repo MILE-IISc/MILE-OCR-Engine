ec99dfb5f9c306fb2e3bb7145021afa3092fd65c
Anil Kumar K K <anilkumar911@gmail.com>
2021-05-27 12:59:51 +0000

    New build environment with tensorflow dev

--- scripts/interactive.sh
+++ scripts/interactive.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name mile-ocr-engine-dev --net=host -v $PWD:/home/ocr mile-ocr-engine-base bash
+docker run -it --rm --name mile-ocr-engine-dev --net=host -v $PWD:/home/ocr mile-ocr-engine-dev bash
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,2 @@
+export LD_LIBRARY_PATH=$PWD/tensorflow-dev/lib/
 ./KannadaClassifier.exe -img input/Kannada-OCR-test-images-with-ground-truth/images/Kan_023_P024.tif input/Kannada-OCR-test-images-with-ground-truth/groundtruth/Kan_023_P024.xml
