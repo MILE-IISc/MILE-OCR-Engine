30521f93d337a04bb93c2118ee2dbf37c4e40c6f
Shiva Kumar H R <shivahr@gmail.com>
2021-05-28 08:49:12 +0530

    Fixes needed for launching Eclipse on DISPLAY=<docker vEthernet>

--- scripts/interactive.sh
+++ scripts/interactive.sh
@@ -1,1 +1,4 @@
+# X applications warn “Couldn't connect to accessibility bus:” on stderr
+export NO_AT_BRIDGE=1
+
 docker run -it --rm --name mile-ocr-engine-dev --net=host -v $PWD:/home/ocr mile-ocr-engine-dev bash
