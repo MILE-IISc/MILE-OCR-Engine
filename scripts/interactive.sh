9c230bf1c57e1b19c34f5fd977216c76bbce9a97
Shiva Kumar H R <shivahr@gmail.com>
2021-05-28 09:19:33 +0530

    Fixes needed for launching Eclipse from within Docker

--- scripts/interactive.sh
+++ scripts/interactive.sh
@@ -1,4 +1,5 @@
 # X applications warn “Couldn't connect to accessibility bus:” on stderr
 export NO_AT_BRIDGE=1
+export DISPLAY=172.27.160.1:0
 
-docker run -it --rm --name mile-ocr-engine-dev --net=host -v $PWD:/home/ocr mile-ocr-engine-dev bash
+docker run -it --rm --name mile-ocr-engine-dev --net=host -v $PWD:/home/ocr mile-ocr-engine-dev -v $PWD/../eclipse-workspace:/root/eclipse-workspace bash
